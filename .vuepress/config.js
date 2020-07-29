74ecb4c070b14659bde954dddb042e1c78cc3ba0
jinzhijie <34389622+jinzhijie@users.noreply.github.com>
2020-07-29 14:32:47 +0800

    add lastUpdated to Vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,22 +1,23 @@
 module.exports = {
-  title: "LittleSkin 社区支持机器人使用手册",
-  description: "LittleSkin 社区支持机器人使用手册",
-  head: [["link", { rel: "icon", href: "/favicon.png" }]],
+  title: 'LittleSkin 社区支持机器人使用手册',
+  description: 'LittleSkin 社区支持机器人使用手册',
+  head: [['link', { rel: 'icon', href: '/favicon.png' }]],
   serviceWorker: true,
   markdown: {
     lineNumbers: true,
-    externalLinks: { target: "_blank" },
+    externalLinks: { target: '_blank' },
   },
   themeConfig: {
-    docsRepo: "LittleSkinCommspt/commspt-bot-manual",
-    docsBranch: "master",
+    docsRepo: 'LittleSkinCommspt/commspt-bot-manual',
+    docsBranch: 'master',
     editLinks: true,
-    editLinkText: "帮助我们完善这个页面",
+    editLinkText: '帮助我们完善这个页面',
     sidebar: [
-      ["/", "欢迎页"],
-      ["/commands", "指令"],
+      ['/', '欢迎页'],
+      ['/commands', '指令'],
     ],
     smoothScroll: true,
+    lastUpdated: '上次更新于',
   },
   evergreen: true,
 };
