267dbdeb5656b879299735e8111ad86f7c3452c9
Nofated <49985975+Nofated095@users.noreply.github.com>
2022-06-18 00:35:08 +0800

    Update config.ts

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -9,28 +9,36 @@ export default defineUserConfig({
   title: 'LittleSkin 社区机器人使用手册',
   description: 'LittleSkin 社区机器人使用手册',
   head: [['link', { rel: 'icon', href: '/favicon.png' }]],
-  serviceWorker: true,
-  markdown: {
-    lineNumbers: true,
-    externalLinks: { target: '_blank' },
-  },
   theme: defaultTheme({
+    docsRepo: 'https://github.com/LittleSkinCommspt/commspt-bot-manual',
+    docsBranch: 'master',
+    editLinkPattern: ':repo/edit/:branch/:path',
+    editLinkText: '帮助我们完善这个页面',
+    lastUpdated: 'true',
+    lastUpdatedText: '上次更新',
     sidebar: [
-      ['/', '欢迎页'],
-      ['/tos', '条例'],
-      ['/commands', '指令'],
-      ['/functions', '功能'],
-      ['/hall-of-jail', 'TOS 违规公告'],
+      {
+        text: '欢迎页',
+        link: '/',
+      },
+      {
+        text: '条例',
+        link: '/tos.md',
+      },
+      {
+        text: '指令',
+        link: '/commands.md',
+      },
+      {
+        text: '功能',
+        link: '/functions.md',
+      },
+      {
+        text: 'TOS 违规公告',
+        link: '/hall-of-jail.md',
+      },
     ],
   }),
-  themeConfig: {
-    docsRepo: 'LittleSkinCommspt/commspt-bot-manual',
-    docsBranch: 'master',
-    editLinks: true,
-    editLinkText: '帮助我们完善这个页面',
-    smoothScroll: true,
-    lastUpdated: '上次更新',
-  },
   plugins: [
     registerComponentsPlugin({
       componentsDir: path.resolve(__dirname, './components'),
